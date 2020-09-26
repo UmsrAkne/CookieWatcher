ba3414ff436bd57c24976fdbf16841560a5ca11a
Akn <sibata486@gmail.com>
2020-09-26 15:01:53 +0900

    機能追加 / ウィンドウにクッキーの現在の枚数を表示するよう実装

--- CookieWatcher/ViewModels/Watcher.cs
+++ CookieWatcher/ViewModels/Watcher.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,22 @@ using System.Threading.Tasks;
 
 namespace CookieWatcher.ViewModels
 {
-    class Watcher
-    {
+    public class Watcher :BindableBase{
+
+        IWebDriver driver;
+
+        public string CookieCount {
+            #region
+            get => cookieCount;
+            set => SetProperty(ref cookieCount, value);
+        }
+
+        private string cookieCount = "0";
+        #endregion
+
+        public Watcher(IWebDriver driver) {
+            this.driver = driver;
+        }
+
     }
 }
