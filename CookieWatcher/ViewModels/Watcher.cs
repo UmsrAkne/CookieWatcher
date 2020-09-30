9b56502066f82e0b0c58d72df71b2778e43dcc9a
Akn <sibata486@gmail.com>
2020-09-30 16:09:45 +0900

    機能追加 / バフがついたらリストビューに表示するよう変更
    バフの種類までは技術的に取得できそうにないので、とりあえず style 属性の値を表示する

--- CookieWatcher/ViewModels/Watcher.cs
+++ CookieWatcher/ViewModels/Watcher.cs
@@ -21,8 +21,33 @@ namespace CookieWatcher.ViewModels
         private string cookieCount = "0";
         #endregion
 
+        public List<string> Buffs {
+            #region
+            get => buffs;
+            private set => SetProperty(ref buffs, value);
+        }
+
+        private List<string> buffs = new List<string>();
+        #endregion
+
         public Watcher(IWebDriver driver) {
             this.driver = driver;
+        }
+
+        /// <summary>
+        /// バフの情報をアップデートします。
+        /// </summary>
+        public void updateBuffs() {
+            var buffsElementList = driver.FindElements(By.CssSelector(".crate.enabled.buff"));
+            var buffsList = new List<string>();
+
+            if(buffsElementList.Count > 0) {
+                foreach(var b in buffsElementList) {
+                    buffsList.Add(b.Text + b.GetAttribute("style"));
+                }
+            }
+
+            Buffs = buffsList;
         }
 
     }
