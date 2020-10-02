4a2a9f70ef9d2e157dc3fabb5efad6b2497a9c76
Akn <sibata486@gmail.com>
2020-10-02 23:57:07 +0900

    クラス追加 / ガーデンの１マスを表すクラスを追加

---
+++ CookieWatcher/Models/GardenTile.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieWatcher.Models
+{
+
+    /// <summary>
+    /// ガーデンの１マスを表します。
+    /// </summary>
+
+    public class GardenTile : BindableBase
+    {
+        public int CropID {
+            #region
+            get => cropID;
+            set => SetProperty(ref cropID, value);
+        }
+
+        private int cropID;
+        #endregion
+
+        public string CropName {
+            #region
+            get => cropName;
+            set => SetProperty(ref cropName, value);
+        }
+
+        public string cropName;
+        #endregion
+
+        public int Level {
+            #region
+            get => level;
+            set => SetProperty(ref level, value);
+        }
+        private int level = 1;
+        #endregion
+
+        public Point Point { get; set; }
+    }
+}
