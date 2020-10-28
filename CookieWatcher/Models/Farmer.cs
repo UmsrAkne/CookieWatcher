71dd109c3b2aa26995b421f667be2546eef70e33
Akn <sibata486@gmail.com>
2020-10-28 14:58:01 +0900

    コード移動 / HarvestCommand を Farmer クラスへ移動
    ガーデン関連の機能であるため、Farmer クラスへ移動する

--- CookieWatcher/Models/Farmer.cs
+++ CookieWatcher/Models/Farmer.cs
@@ -1,9 +1,11 @@
 ﻿using OpenQA.Selenium;
+using Prism.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace CookieWatcher.Models {
 
@@ -18,5 +20,19 @@ namespace CookieWatcher.Models {
         public Farmer(IWebDriver webDriver) {
             driver = webDriver;
         }
+
+        public DelegateCommand<ListViewItem> HarvestCommand {
+            #region
+            get => harvestCommand ?? (harvestCommand = new DelegateCommand<ListViewItem>((ListViewItem param) => {
+                var gardenTile = param.Content as GardenTile;
+                if (gardenTile != null && driver.FindElement(By.Id(gardenTile.CropIDName)).Displayed) {
+                    driver.FindElement(By.Id(gardenTile.CropIDName)).Click();
+                }
+            }));
+        }
+
+        private DelegateCommand<ListViewItem> harvestCommand;
+        #endregion
+
     }
 }
--- CookieWatcher/ViewModels/MainWindowViewModel.cs
+++ CookieWatcher/ViewModels/MainWindowViewModel.cs
@@ -108,17 +108,5 @@ namespace CookieWatcher.ViewModels
         #endregion
 
 
-        public DelegateCommand<ListViewItem> HarvestCommand {
-            #region
-            get => harvestCommand ?? (harvestCommand = new DelegateCommand<ListViewItem>((ListViewItem param) => {
-                var gardenTile = param.Content as GardenTile;
-                if (gardenTile != null && driver.FindElement(By.Id(gardenTile.CropIDName)).Displayed) {
-                    driver.FindElement(By.Id(gardenTile.CropIDName)).Click();
-                }
-            }));
-        }
-
-        private DelegateCommand<ListViewItem> harvestCommand;
-        #endregion
     }
 }
